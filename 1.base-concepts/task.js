a3fccf85244022a89858ddb8bed76e518ddd8ddb
Trofimov.M <milllky@rocketmail.com>
2025-08-19 11:26:37 +0500

    fixed task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,25 +1,26 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = b ** 2 - 4 * a * c;
-  if (d < 0) {
-    return arr;
-  } else if ( d === 0) {
-    arr.push (-b/( 2 * a));
-  } else {
-    arr.push ((-b + Math.sqrt(d) )/(2*a));
-    arr.push ((-b - Math.sqrt(d) )/(2*a));
-  }
-  return arr;
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+	if (d < 0) {
+		return arr;
+	} else if (d === 0) {
+		arr.push(-b / (2 * a));
+	} else {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let monthPer = (percent/100)/12;
-  let crBody = amount - contribution;
-  if (crBody <= 0) {
-    return 0;
-  }
-  let monthPay = crBody * (monthPer + (monthPer/(((1 + monthPer)**countMonths) - 1)));
-  let totalPay = monthPay * countMonths;
-  return Math.round(totalPay * 100)/100
+	let monthPer = (percent / 100) / 12;
+	let crBody = amount - contribution;
+	if (crBody <= 0) {
+		return 0;
+	}
+	let monthPay = crBody * (monthPer + (monthPer / (((1 + monthPer) ** countMonths) - 1)));
+	let totalPay = monthPay * countMonths;
+	return Math.round(totalPay * 100) / 100
 }
